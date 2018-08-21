d0877fbc24af32bba1309be22a882284616bd6b5
t-bast <bastien@stratumn.com>
2018-08-21 16:41:07 +0200

    link: basic link builder that adds version, process and map id

--- src/link.spec.ts
+++ src/link.spec.ts
@@ -1,11 +1,26 @@
-import { linkVersion } from "./link";
-import { Link } from "./proto/chainscript_pb";
+import { Link } from "./link";
+import { Link as PbLink, LinkMeta as PbLinkMeta } from "./proto/chainscript_pb";
 
 describe("link", () => {
-  it("version", () => {
-    const link = new Link();
-    link.setVersion("0.42.0");
+  describe("process", () => {
+    it("throws if meta is missing", () => {
+      const link = new Link(new PbLink());
+      expect(() => link.process()).toThrowError("link meta is missing");
+    });
 
-    expect(linkVersion(link)).toEqual("0.42.0");
+    it("throws if process is missing", () => {
+      const pbLink = new PbLink();
+      pbLink.setMeta(new PbLinkMeta());
+
+      const link = new Link(pbLink);
+      expect(() => link.process()).toThrowError("link process is missing");
+    });
+  });
+
+  describe("map id", () => {
+    it("throws if meta is missing", () => {
+      const link = new Link(new PbLink());
+      expect(() => link.mapId()).toThrowError("link meta is missing");
+    });
   });
 });
