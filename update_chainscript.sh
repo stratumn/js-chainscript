e9fb20cb7577544883b02c1a437cb51cd975e2b1
t-bast <bastien@stratumn.com>
2018-09-17 11:50:42 +0200

    update_chainscript: redirect errors

--- update_chainscript.sh
+++ update_chainscript.sh
@@ -4,12 +4,11 @@
 # You can also run this from npm: `npm run update_chainscript`
 
 if [[ -n $(git diff --stat) ]]; then
-    echo "Chainscript cannot be updated: you have unstaged changes in your working tree. Please commit or stash your current work and retry."
+    >&2 echo "Chainscript cannot be updated: you have unstaged changes in your working tree. Please commit or stash your current work and retry."
 elif [[ -n $(git status -s) ]]; then
-    echo "Chainscript cannot be updated: you have staged changes. Please commit or stash your current work and retry."
+    >&2 echo "Chainscript cannot be updated: you have staged changes. Please commit or stash your current work and retry."
 else
     rm -r ./proto
-    git add ./proto
-    git commit -m "removing old chainscript subtree"
+    git commit -m -a "removing old chainscript subtree"
     git subtree add --prefix proto git@github.com:stratumn/chainscript.git master --squash
 fi
